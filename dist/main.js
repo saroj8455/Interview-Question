d76c875ff98bab95298cda1702484d067896822b
Saroj Padhan <saroj.padhan11@gmail.com>
2021-01-02 08:58:30 +0530

    code for fetch api

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,42 @@
 const laptop = {
-    brand:'Apple',
-    year:2017
+  brand: "Apple",
+  year: 2017,
+};
+
+console.log(laptop);
+
+/**
+ * @Saroj
+ * Fetch api
+ */
+
+const usersDetils = () => {
+  let users = fetch("https://jsonplaceholder.typicode.com/users")
+    .then((userResp) => userResp.json())
+    .then((data) => data);
+  console.log(users);
+};
+
+usersDetils();
+
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
 }
 
-console.log(laptop)
+function json(response) {
+  return response.json();
+}
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(status)
+  .then(json)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
